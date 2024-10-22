b00ff32c3b1f42445682e348468e9771898c8bbc
Juliette Hutton <juliettehutton2@gmail.com>
2024-10-22 14:41:57 -0400

    refactored with Sorter as an abstract class

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,11 @@ import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([50, 34, -5, 0]);
-const sorterNum = new Sorter(numbersCollection);
-sorterNum.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('Hi There!')
-const sorterChar = new Sorter(charactersCollection);
-sorterChar.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedListCollection = new LinkedList();
@@ -19,6 +17,5 @@ linkedListCollection.add(-10);
 linkedListCollection.add(-5);
 linkedListCollection.add(4);
 
-const sorterLinkedList = new Sorter(linkedListCollection);
-sorterLinkedList.sort();
+linkedListCollection.sort()
 linkedListCollection.print();
