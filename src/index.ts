6bb2cc7c398966709d1f234bfe3b2466fc83b67f
Juliette Hutton <juliettehutton2@gmail.com>
2024-10-22 14:10:15 -0400

    added logic for linked lists

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([50, 34, -5, 0]);
 const sorterNum = new Sorter(numbersCollection);
@@ -11,3 +12,13 @@ const charactersCollection = new CharactersCollection('Hi There!')
 const sorterChar = new Sorter(charactersCollection);
 sorterChar.sort();
 console.log(charactersCollection.data);
+
+const linkedListCollection = new LinkedList();
+linkedListCollection.add(500);
+linkedListCollection.add(-10);
+linkedListCollection.add(-5);
+linkedListCollection.add(4);
+
+const sorterLinkedList = new Sorter(linkedListCollection);
+sorterLinkedList.sort();
+linkedListCollection.print();
