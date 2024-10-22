6b7f14436e5e8f4ecd892a3b8dded4f1e061c85b
Juliette Hutton <juliettehutton2@gmail.com>
2024-10-22 19:14:09 -0400

    tested

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
-const numbersCollection = new NumbersCollection([50, 34, -5, 0]);
+const numbersCollection = new NumbersCollection([14, 2, 550, 69, 420]);
 numbersCollection.sort();
 console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('Hi There!')
+const charactersCollection = new CharactersCollection('Paco est à la pizzeria')
 charactersCollection.sort();
 console.log(charactersCollection.data);
 
